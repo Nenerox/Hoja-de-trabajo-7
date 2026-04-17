46b327e559cdb43452c4f18ec10deabe19c0ae26
Nenerox <andres.ps0308@gmail.com>
2026-04-17 16:36:06 -0600

    changed binarytree to null version

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,65 +1,37 @@
 public class BinaryTree<E> {
     protected E val; 
-	protected BinaryTree<E> parent; 
 	protected BinaryTree<E> left, right;
 	
-	public BinaryTree()
-	{
-		val = null;
-		parent = null; left = right = this;
-	}
-	
-	public BinaryTree(E value)
-	{
-		val = value;
-		right = left = new BinaryTree<E>();
-		setLeft(left);
-		setRight(right);
-	}
-	
-	public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right)
-	{
-		val = value;
-		if (left == null) { left = new BinaryTree<E>(); }
-		setLeft(left);
-		if (right == null) { right = new BinaryTree<E>(); }
-		setRight(right);
-	}
+	public BinaryTree(E value) {
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right) {
+        this.val = value;
+        this.left = left;
+        this.right = right;
+    }
 	
 	public BinaryTree<E> left()
 	{
 		return left;
 	}
 	
-	public BinaryTree<E> parent()
-	{
-		return parent;
-	}
 	public BinaryTree<E> right()
 	{
 		return right;
 	}
+	
 	public void setLeft(BinaryTree<E> newLeft)
 	{
-		if (isEmpty()) return;
-		if (left != null && left.parent() == this) left.setParent(null);
-		left = newLeft;
-		left.setParent(this);
+		this.left = newLeft;
 	}
 
 	public void setRight(BinaryTree<E> newRight)
 	{
-		if (isEmpty()) return;
-		if (right != null && right.parent() == this) right.setParent(null);
-		right = newRight;
-		right.setParent(this);
-	}
-	
-	protected void setParent(BinaryTree<E> newParent)
-	{
-		if (!isEmpty()) {
-		parent = newParent;
-		}
+		this.right = newRight;
 	}
 	
 	public E value()
@@ -69,15 +41,12 @@ public class BinaryTree<E> {
 	
 	public void setValue(E value)
 	{
-		val = value;
+		this.val = value;
 	}
 
 	public boolean isEmpty()
 	{
-		if (val != null) {
-			return true;
-		}
-		return false;
+		return val == null;
 	}
 	
 }
