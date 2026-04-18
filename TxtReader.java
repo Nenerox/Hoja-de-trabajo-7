aeb21a101867455e4bc84d38c766626f5e75ec07
Nenerox <andres.ps0308@gmail.com>
2026-04-17 18:25:44 -0600

    Complete reader of .txt to string[]

--- TxtReader.java
+++ TxtReader.java
@@ -1,3 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class TxtReader {
     
+    public String[] readFile(String filePath, String delimiter) {
+        String[] lista = new String[0];
+        try {
+            lista = Files.readString(Path.of(filePath)).trim().split(delimiter);
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return lista;
+    }
 }
